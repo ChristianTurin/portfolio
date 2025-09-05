5355a01908f42a5197554978f9b40b791997fa79
christianTurin <iiuknown56ii@gmail.com>
2025-09-05 18:29:14 -0500

    preparando para subir

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configuración básica para cPanel
   poweredByHeader: false,
   generateEtags: false,
-  
-  // Si tienes problemas con imágenes
-  images: {
-    domains: ['localhost', 'kaiserdevs.com'],
-  },
-  
-  // Para mejor compatibilidad
   experimental: {
     serverComponentsExternalPackages: ['nodemailer']
   }
